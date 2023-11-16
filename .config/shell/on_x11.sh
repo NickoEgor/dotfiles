35851fc13904d641a7a07e74f71938e224bfb751
NickoEgor <egor1998nick@gmail.com>
2023-11-16 06:36:28 -0800

    November rains

--- .config/shell/on_x11.sh
+++ .config/shell/on_x11.sh
@@ -1,6 +1,14 @@
-dunst &
-compton &
-setwall &
-sxhkd &
-greenclip daemon &
-xboard.sh &
+#!/bin/bash
+
+autostart=(
+    "dunst"
+    "compton"
+    "setwall"
+    "sxhkd"
+    "greenclip daemon"
+    "xboard.sh"
+)
+
+for program in "${autostart[@]}"; do
+    pidof -sx "$(echo "$program" | cut -d' ' -f1)" || $program &
+done >/dev/null 2>&1
